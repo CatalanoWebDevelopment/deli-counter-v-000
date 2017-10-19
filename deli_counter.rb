6709baa7cabd47b8d28e74f6feeaf17ee47bbe07
Parker Catalano <catalano.work@gmail.com>
2017-10-19 19:55:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-      new_array = katz_deli.map_with_index do |name, index|
+      new_array = katz_deli.map.with_index do |name, index|
       "#{index + 1}. #{name}"
     end
     new_array.join(", ").unshift("There")
