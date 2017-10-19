fa8078576e7d85e4d267d0a3c9b01b19b052f72c
Parker Catalano <catalano.work@gmail.com>
2017-10-19 19:19:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,9 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    katz_deli.map do |people|
-    puts "The line is currently: #{katz_deli}"
-    binding.pry
+      katz_deli.map do |people|
+      puts "The line is currently: #{katz_deli}"
+      binding.pry
+    end
   end
 end
