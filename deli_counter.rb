d2c7c535b8822d92302818fd2e45dca77ed7f645
Parker Catalano <catalano.work@gmail.com>
2017-10-19 20:02:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,6 @@ def line(katz_deli)
       new_array = katz_deli.map.with_index do |name, index|
       "#{index + 1}. #{name}"
     end
-    new_array.join(", ")
+    new_array.join(", ").unshift("The line is currently: ")
   end
 end
